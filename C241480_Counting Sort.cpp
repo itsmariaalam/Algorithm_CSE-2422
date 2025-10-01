2ad289af620867625b7b7ff491e16ad4569ee3a2
Maria Alam <memariaalam@gmail.com>
2025-10-01 16:40:05 +0600

    added C241480_Counting Sort.cpp file

---
+++ C241480_Counting Sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[100];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > maxVal) maxVal = arr[i];
+    int count[100] = {0};
+    for (int i = 0; i < n; i++)
+        count[arr[i]]++;
+    cout << "Sorted array: ";
+    for (int i = 0; i <= maxVal; i++) {
+        while (count[i] > 0) {
+            cout << i << " ";
+            count[i]--;
+        }
+    }
+    cout << endl;
+    return 0;
+}
+
